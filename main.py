5ae05b173247a273444e7c6c681ef2565d584da7
Chirag <wavespace6@gmail.com>
2023-10-12 19:34:52 +0530

    Data Ingestion

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.mlproject import logger
+from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
